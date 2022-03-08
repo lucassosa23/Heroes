bc49e32f75331d9951038f2792c12c44c2e6dcef
Lucas <lucassosa4@hotmail.com>
2022-03-08 13:35:40 -0300

    navbar

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -25,20 +25,20 @@ export const Navbar = () => {
             <div className="navbar-nav">
 
                     <NavLink 
-                        className={({isActive})=> "nav-item nav-link" + (isActive ? "active" : "")} 
+                        className={({isActive})=> " navbar-brand nav-item nav-link" + (isActive ? "active" : "")} 
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink 
-                        className={({isActive})=> "nav-item nav-link" + (isActive ? "active" : "")}
+                        className={({isActive})=> " navbar-brand nav-item nav-link" + (isActive ? "active" : "")}
                         to="/dc"
                     >
                         DC
                     </NavLink>
                     <NavLink 
-                        className={({isActive})=> "nav-item nav-link" + (isActive ? "active" : "")}
+                        className={({isActive})=> " navbar-brand nav-item nav-link" + (isActive ? "active" : "")}
                         to="/search"
                     >
                         Search
